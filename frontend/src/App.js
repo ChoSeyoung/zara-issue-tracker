67bd42161dce853f71a2a596add80b67d67e016f
ChoSeyoung <a01075511157@gmail.com>
2024-08-15 12:45:48 +0900

    init

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Login from "./pages/Login/Login";
+import {AuthProvider} from "./contexts/AuthContext";
+import Home from "./pages/Home/Home";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
-  return (
-      <div className="text-center">
-        <header className="bg-blue-500 text-white p-6">
-          <h1 className="text-xl">Hello, Tailwind CSS!</h1>
-        </header>
-      </div>
-  );
+    return (
+        <AuthProvider>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Login/>}/>
+                    <Route path="/home" element={<PrivateRoute element={Home} />} />
+                </Routes>
+            </Router>
+        </AuthProvider>
+    );
 }
 
 export default App;
---
+++ frontend/src/components/PrivateRoute.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
+
+function PrivateRoute({ element: Component }) {
+    const { isAuthenticated } = useAuth();
+
+    return isAuthenticated ? <Component /> : <Navigate to="/" />;
+}
+
+export default PrivateRoute;
